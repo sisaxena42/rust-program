abf57b2b91bdcadd3a045f5e2fcdef4f6e6cee41
sisaxena42 <sisaxena@ad.unc.edu>
2025-01-26 22:11:52 +0000

    fixed issue with wd

---
+++ hello_comp423/target/debug/deps/hello_comp423-4d1ea95a1e49f936.d
@@ -0,0 +1,5 @@
+/workspaces/rust-setup-proj/hello_comp423/target/debug/deps/hello_comp423-4d1ea95a1e49f936: src/main.rs
+
+/workspaces/rust-setup-proj/hello_comp423/target/debug/deps/hello_comp423-4d1ea95a1e49f936.d: src/main.rs
+
+src/main.rs:
---
+++ hello_comp423/target/debug/hello_comp423.d
@@ -0,0 +1,1 @@
+/workspaces/rust-setup-proj/hello_comp423/target/debug/hello_comp423: /workspaces/rust-setup-proj/hello_comp423/src/main.rs
